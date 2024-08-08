3e94f72691d1292ed4f7bdbdf8eb891cfc376d24
eobiki <eobiki@rythmia.com>
2024-08-08 16:53:37 -0600

    Code clean up

--- vagrant/ubuntu/kubeadm/kubemaster-init.sh
+++ vagrant/ubuntu/kubeadm/kubemaster-init.sh
@@ -71,10 +71,6 @@ EOF
    curl -fsSL https://pkgs.k8s.io/core:/stable:/v$KUBERNETES_VERSION/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
    echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v$KUBERNETES_VERSION/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-#    curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-#    cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
-#    deb https://apt.kubernetes.io/ kubernetes-xenial main
-# EOF
    sudo apt-get update
    sudo apt-get install -y kubelet kubectl kubeadm
    sudo apt-mark hold kubelet kubeadm kubectl
